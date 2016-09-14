13be1e790e5ebc99d212bc2aceafa09b853a80e8
nash-md <mdamm@twilio.com>
2016-09-14 21:17:46 +0200

    load configuration instead of copy

--- setup-database.js
+++ setup-database.js
@@ -1,5 +1,6 @@
-var async 	= require('async')
-var pg    	= require('pg')
+const async 	= require('async')
+const pg    	= require('pg')
+const fs 			= require('fs')
 
 async.waterfall([
 
@@ -7,69 +8,63 @@ async.waterfall([
 
 		pg.connect(process.env.DATABASE_URL, function (err, client, done) {
 			if (err) {
-				console.log(err)
-				process.exit(1)
+				callback(err)
 			} else {
 				callback(null, client)
 			}
 		})
 
-	},
-	function (client, callback) {
+	}, function (client, callback) {
 
-		client.query('CREATE TABLE IF NOT EXISTS configuration (id serial, data text)', function (err, result) {
-			if (err) {
-				console.log(err)
-				process.exit(1)
-			} else {
-				callback(null, client)
+		client.query('CREATE TABLE IF NOT EXISTS configuration (id serial, data text)',
+			function (err, result) {
+				if (err) {
+					callback(err)
+				} else {
+					callback(null, client)
+				}
 			}
-		})
+		)
 
-	},
-	function (client, callback) {
+	}, function (client, callback) {
 
 		client.query('TRUNCATE configuration', function (err, result) {
 			if (err) {
-				console.log(err)
-				process.exit(1)
+				callback(err)
 			} else {
 				callback(null, client)
 			}
 		})
 
-	},
-	function (client, callback) {
-		var configuraton =  {
-			ivr: { text: 'Welcome to my call center, please press 1 for sales, press 2 for support', options: [{ friendlyName: 'Sales', digit: 1, id: 'sales'}, { friendlyName: 'Support', digit: 2, id: 'support'}]},
-			queues: [{ friendlyName: 'Email Queue', id: 'email', taskQueueSid: null, expression: 'channel == "email"'},
-					{ friendlyName: 'Chat Queue', id: 'chat', taskQueueSid: null, expression: 'channel == "chat"'},
-					{ friendlyName: 'Phone Queue', id: 'phone', taskQueueSid: null, expression: 'channel == "phone"'},
-					{ friendlyName: 'Video Queue', id: 'phone', taskQueueSid: null, expression: 'channel == "video"'}],
-			twilio: {
-				workerOfflineActivitySid: null,
-				workerIdleActivitySid: null,
-				workerReservationActivitySid: null,
-				workerAssignmentActivitySid: null,
-				facbookPageId: null
-			}
-		}
+	}, function (client, callback) {
 
-		client.query('INSERT INTO configuration(data) values($1)', [JSON.stringify(configuraton)], function (err, result) {
-			if (err) {
-				console.log(err)
-				process.exit(1)
-			} else {
-				callback(null, 'tables successfully created')
+		/* load configuration template */
+		fs.readFile('./configuration.json', 'utf8', function (err, data) {
+		  if (err) {
+		    callback(err)
+		  }
+		  callback(null, client, data)
+		});
+
+	}, function (client configuration, callback) {
+		
+		client.query('INSERT INTO configuration(data) values($1)', [configuraton],
+			function (err, result) {
+				if (err) {
+					callback(err)
+				} else {
+					callback(null, 'table successfully created, configuration saved')
+				}
 			}
-		})
+		)
+
 	}
 ],function (err, message) {
 	if (err) {
 		console.log(err)
+		process.exit(1)
 	} else {
 		console.log(message)
 	}
 	process.exit(0)
 })
-
