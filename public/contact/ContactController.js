e09fc7a143b2c06ec3c6b035c2ecc78785a3f938
nash-md <mdamm@twilio.com>
2016-09-15 18:49:43 +0200

    put angular directives into separate folder

--- public/contact/ContactController.js
+++ public/contact/ContactController.js
@@ -8,7 +8,6 @@ app.controller('ContactController', function ($scope, $http, $timeout, $log) {
 	$scope.init = function(){
 
 		$http.get('/api/setup')
-
 			.then(function onSuccess(response) {
 				$scope.configuration = response.data;
 			}, function onError(response) { 
@@ -52,30 +51,4 @@ app.controller('ContactController', function ($scope, $http, $timeout, $log) {
 
 	};
 
-});
-
-app.directive('phoneNumber', function () {
-
-	var pattern = /^\+[0-9]{8,20}$/;
-
-	return {
-		require: 'ngModel',
-		link: function (scope, element, attrs, ctrl) {
-
-			ctrl.$validators.integer = function (ngModelValue) {
-				if(ngModelValue == undefined || ngModelValue == null){
-					ctrl.$setValidity('invalidPhone', true);
-					return ngModelValue;
-				}
-				if (pattern.test(ngModelValue) == false) {
-					ctrl.$setValidity('invalidPhone', false);
-					return ngModelValue;
-				}
-
-				ctrl.$setValidity('invalidPhone', true);
-				return ngModelValue;
-
-			};
-		}
-	};
 });
