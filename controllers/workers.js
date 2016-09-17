bb3edb0bf04bfa9c37ae9a3f0e42a9238913d213
nash-md <mdamm@twilio.com>
2016-09-17 12:11:34 +0200

    workers controller with promises

--- controllers/workers.js
+++ controllers/workers.js
@@ -10,39 +10,38 @@ const taskrouterClient = new twilio.TaskRouterClient(
 
 module.exports.delete = function (req, res) {
 
-	taskrouterClient.workspace.workers(req.params.id).delete(null, function (err) {
-		if (err) {
-			res.status(500).json(err)
-		} else {
+	taskrouterClient.workspace.workers(req.params.id).delete(null)
+		.then(function (result) {
 			res.status(200).end()
-		}
-	})
+		}).catch(function (err) {
+			res.status(500).json(err)
+		})
 
 }
 
 module.exports.create = function (req, res) {
 
-	taskrouterClient.workspace.workers.create({
+	var worker = {
 		friendlyName: req.body.friendlyName,
 		attributes: req.body.attributes
-	}, function (err) {
-		if (err) {
+	}
+
+	taskrouterClient.workspace.workers.create(worker)
+		.then(function (worker) {
+			res.status(200).json(worker)
+		}).catch(function (err) {
 			res.status(500).json(err)
-		} else {
-			res.status(200).end()
-		}
-	})
+		})
 
 }
 
 module.exports.list = function (req, res) {
 
-	taskrouterClient.workspace.workers.list(function (err, data) {
-		if (err) {
-			res.status(500).json(err)
-		} else {
+	taskrouterClient.workspace.workers.list()
+		.then(function (data) {
 			res.status(200).json(data.workers)
-		}
-	})
+		}).catch(function (err) {
+			res.status(500).json(err)
+		})
 
 }
